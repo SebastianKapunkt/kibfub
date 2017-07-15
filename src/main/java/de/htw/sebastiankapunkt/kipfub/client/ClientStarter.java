9c821b7ce1d621cf41ae9ff577db6319728e8f0a
Sebastian Kapunkt <SebastianKapunkt@users.noreply.github.com>
2017-07-15 16:27:52 +0200

    Server Configuration

--- src/main/java/de/htw/sebastiankapunkt/kipfub/client/ClientStarter.java
+++ src/main/java/de/htw/sebastiankapunkt/kipfub/client/ClientStarter.java
@@ -4,14 +4,14 @@ public class ClientStarter {
     public static void main(String[] args) {
         String host = "192.168.2.100";
 
+//        new Thread(() -> {
+//           new KipFubClient(host, "Name1");
+//        }).start();
         new Thread(() -> {
-            new KipFubClient(host, "Name1");
+            new RandomClient(host, "Random1");
         }).start();
-//        new Thread(() -> {
-//            new RandomClient(host, "Random1");
-//        }).start();
-//        new Thread(() -> {
-//            new RandomClient(host, "Random2");
-//        }).start();
+        new Thread(() -> {
+            new RandomClient(host, "Random2");
+        }).start();
     }
 }
